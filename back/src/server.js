b5a8c33ddc549be73634345de35ce9a9b0aaedcb
Lucas Guerendiain <lucasguere@hotmail.com>
2023-03-08 13:20:26 -0300

    acomodando server

--- back/src/server.js
+++ back/src/server.js
@@ -1,28 +1,20 @@
-var http = require("http");
-var fs = require("fs");
-var data = require("./utils/data.js");
+const http = require("http");
+const fs = require("fs");
+var characters = require("./utils/data.js");
 
 const PORT = 3001;
 
-module.exports = 
-    http.createServer((req, res) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        if (req.url.includes("rickandmorty/character")) {
-            let id = req.url.split("/").pop();
-            /*fs.readFile(__dirname + "/utils/data.js", "utf-8", (err, data) => {
-                if (err) {
-                    res.writeHead(404, { "Content-Type" : "text/plain" });
-                    res.end("Route not found");
-                }
-                let char = data.filter((char) => char.id === id);
-                res.writeHead(200 ,{ "Content-Type" : "application/json" });
-                res.end(char);
-            });*/
-            let char = data.filter(char => char.id == id);
-            res.writeHead(200 ,{ "Content-Type" : "application/json" });
-            res.end(JSON.stringify(char));
-            return
-        }
-        res.writeHead(404, { "Content-Type" : "text/plain" });
-        res.end("Route not found");
-    }).listen(PORT, "localhost");
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (req.url.includes("rickandmorty/character")) {
+        const id = req.url.split("/").pop();
+        let char = characters.filter(char => char.id === Number(id));
+        res
+        .writeHead(200 ,{ "Content-Type" : "application/json" })
+        .end(JSON.stringify(char[0]));
+        return
+    }
+    res
+    .writeHead(404, { "Content-Type" : "text/plain" })
+    .end("Route not found");
+}).listen(PORT, "localhost");
